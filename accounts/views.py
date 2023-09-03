d82e8b34ad3a4611f9b65f8a1d1478b4179562ae
Emi13ach <ebach.magic@gmail.com>
2023-09-03 15:47:43 +0200

    add accounts and authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,66 @@
 from django.shortcuts import redirect, render
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 def register(request):
-    return render(request, "accounts/register.html")
+    if request.method == "POST":
+        # Get form values
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        password2 = request.POST["password2"]
+        
+        # Check if passwords not match
+        if password == password2:
+            # Check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, "That username is taken!")
+                return redirect("register")
+            # Check email
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, "That email address is being used!")
+                return redirect("register")
+            else:
+                # Validation is correct
+                user = User.objects.create_user(
+                    first_name=first_name,
+                    last_name=last_name,
+                    username=username,
+                    email=email,
+                    password=password
+                    )
+                user.save()
+                messages.success(request, "You are now registered!")
+                return redirect("login") 
+        else:
+            messages.error(request, "Passwords do not match!")
+            return redirect("register")     
+            
+    else:
+        return render(request, "accounts/register.html")
 
 def login(request):
-    return render(request, "accounts/login.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, "You are now logged in!")
+            return redirect("dashboard")
+        else:    
+            messages.error(request, "Invalid credentials!")
+            return redirect("login")
+    else:
+        return render(request, "accounts/login.html")
 
 def logout(request):
-    return redirect("index")
+    if request.method == "POST":
+        auth.logout(request)
+        messages.success(request, "You are logged out!")
+        return redirect("index")
 
 def dashboard(request):
     return render(request, "accounts/dashboard.html")
--- btre/settings.py
+++ btre/settings.py
@@ -10,9 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+
 import os
 from pathlib import Path
 from decouple import config
+from django.contrib.messages import constants as messages
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,3 +143,9 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
